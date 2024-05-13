4f1d2ff66c389595b4b5ec690efcfb384fb53d09
EvrettG <skyrift@tpg.com.au>
2024-05-13 17:17:04 +1000

    can now enter a valid color ot valid hexidecimal number for color

--- Index.js
+++ Index.js
@@ -4,9 +4,10 @@ const colors = require('colors');
 // used for writting to file functions
 const fs = require('fs');
 // Used to load shapes.js for making the svg shapes
-// const shapes = require("./lib/shapes");
+const shapes = require("./lib/shapes");
+const colorList = require("./data/colorList");
 
-const fileName = "./lib/logo.svg"
+const fileName = "./examples/logo.svg"
 
 // Function that write's the svg logo to file
 function writeToFile(data) {
@@ -14,8 +15,17 @@ function writeToFile(data) {
   err ? console.error(err) : console.log('.svg successfully made!')
 );
 }
-
+// Used as part of the validation of colors selected limiting to hexidecimal range and 3 - 6 charachters, while requiring to start with a #
+const hexRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
 // This variable stores all the questions for inquirer prompt
+function validation(data) {
+        if (colorList.includes(data) || hexRegex.test(data)){
+            return true;
+        } else {
+            return "This color is an invalid selection or is not a valid hexadcimal color code which is somewhere between #000000 and #FFFFFF"
+        }
+}
+
 const questions = [
     {
         type: 'input',
@@ -33,10 +43,10 @@ const questions = [
     },
     },
     {
-        type: 'list',
-        message: colors.brightGreen('Please Select a color for your title'),
+        type: 'input',
+        message: colors.brightGreen('Please type a color for your title or a valid hexidecimal code, that must start with a # and 3 - 6 chacters between 0-9 and a-f'),
         name: "titleColor",
-        choices: ["red", "yellow", "blue", "green", "black", "white"],
+        validate: validation
     },
     {
         type:`list`,
@@ -45,24 +55,25 @@ const questions = [
         choices: ["square","triangle","circle"]
     },
     {
-        type: 'list',
-        message: colors.brightGreen('Please Select a color for your shape'),
+        type: 'input',
+        message: colors.brightGreen('Please type a color for your title or a valid hexidecimal code, that must start with a # and 3 - 6 chacters between 0-9 and a-f'),
         name: "shapeColor",
-        choices: ["red", "yellow", "blue", "green", "black", "white"],
+        validate: validation
     },
     {
-        type: 'list',
-        message: colors.brightGreen('Please Select a color for your background'),
+        type: 'input',
+        message: colors.brightGreen('Please type a color for your title or a valid hexidecimal code, that must start with a # and 3 - 6 chacters between 0-9 and a-f'),
         name: "backgroundColor",
-        choices: ["red", "yellow", "blue", "green", "black", "white"],
+        validate: validation
     },
 ]
 
 // Function called when program starts
 function init(){
-    inquirer.prompt(questions).then((data) =>
-//         // writeToFile(generateMarkdown(data))
+    inquirer.prompt(questions).then((data) => {
+        writeToFile(shapes(data))
         console.log(data)
+    }
 )
 }
 // used to start program. Must remain bottom
